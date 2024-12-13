22547f6c68d5541752187014c4072f690d049718
KTD1090 <itskrutidesai@gmail.com>
2024-12-13 17:26:19 -0500

    add new file

--- 2024/weight_converter.py
+++ 2024/weight_converter.py
@@ -1,2 +1,9 @@
-def greet():
-    print('Hello, World!')
+weight=int(input('weight:'))
+unit=input('(L)lbs or (K)g:)')
+
+if unit.upper()=="L":
+    converted=weight*0.45
+    print(f"you are {converted} kilos")
+else:
+    converted=weight/0.45
+    print(f"you are {converted} lbs")
