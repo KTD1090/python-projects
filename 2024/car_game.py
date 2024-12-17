12660d9fd14b53e58a0cf891ad45283a74dfaf5b
KTD1090 <itskrutidesai@gmail.com>
2024-12-17 11:15:59 -0500

    add new file

---
+++ 2024/car_game.py
@@ -0,0 +1,9 @@
+user_input=input(">")
+if user_input.lower()== "start":
+    print("car started")
+elif user_input.lower()=="stop":
+    print("car stopped")
+elif user_input.lower()=="quit":
+    quit
+else:
+    print("sorry,i don't understand the message")
